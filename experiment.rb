52214e829e55b3fe03b0ad4d13cd854b864b307a
Andrew Brown <drusepth@gmail.com>
2020-07-18 02:17:15 -0700

    actually, stick to .txt

--- experiment.rb
+++ experiment.rb
@@ -52,15 +52,9 @@ end
   puts "Completion result:"
   puts completions.join(settings.fetch(:result_delimiter))
 
-  # Store results
-  filename = "experiments/#{experiment_name}/output-#{temperature.to_s.gsub('.', '_')}"
-
-  # We started with .txt files before switching to .md (so GitHub would do line wrapping!), so we should
-  # go ahead and clear any .txt files of the same name here also.
-  File.delete("#{filename}.txt") if File.exist?("#{filename}.txt")
-
   # Write the completions for this temperature to an output file
-  File.write("#{filename}.md", completions.join(settings.fetch(:result_delimiter)))
+  filename = "experiments/#{experiment_name}/output-#{temperature.to_s.gsub('.', '_')}.txt"
+  File.write("#{filename}", completions.join(settings.fetch(:result_delimiter)))
 
   # Sleep for a short period of time to keep stress off OpenAI servers
   puts "Briefly sleeping -- hold tight!"
